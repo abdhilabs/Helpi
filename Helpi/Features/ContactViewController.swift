4b949b49a0b24e8cf335b1209662c7b6541f5705
Abdhilabs <abdhi@nusantarabetastudio.com>
2022-07-05 12:10:57 +0700

    adjust on contact

--- Helpi/Features/ContactViewController.swift
+++ Helpi/Features/ContactViewController.swift
@@ -86,8 +86,7 @@ class ContactViewController: UIViewController, CNContactPickerDelegate  {
   func showAlert(){
     let alert = UIAlertController(title: "The Field Couldn’t Empty", message: "Need to input at least 1 Emergency Contact", preferredStyle: .alert)
 
-    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [self] action in
-    }))
+    alert.addAction(UIAlertAction(title: "OK", style: .default))
 
     present(alert, animated: true)
   }
@@ -104,7 +103,7 @@ class ContactViewController: UIViewController, CNContactPickerDelegate  {
 
   func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
     let name = contact.givenName + " " + contact.familyName
-    let number = ((contact.phoneNumbers.first?.value)! as CNPhoneNumber).stringValue
+    let number = ((contact.phoneNumbers.first?.value) as? CNPhoneNumber)?.stringValue
     switch contactFrom {
     case .contactPrimary:
       fullName1.text = name
@@ -119,8 +118,7 @@ class ContactViewController: UIViewController, CNContactPickerDelegate  {
   func showAlertContact1(){
     let alert = UIAlertController(title: "“Helpi” Would Like to Access Your Contacts", message: "Upload your emergency contacts to Helpi so first responders know who to reach out in case of emergency.", preferredStyle: .alert)
 
-    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { [self] action in
-    }))
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
 
     alert.addAction(UIAlertAction(title: "OK", style: .default , handler: {  action in
       self.contactFrom = .contactPrimary
